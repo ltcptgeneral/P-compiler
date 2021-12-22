da64ef207171b0eb8785f717b8d47eab77b0a9a5
Arthur Lu <learthurgo@gmail.com>
2021-12-22 02:39:06 +0000

    added bf IO symbols, added simple bracket balance checking, fixed file io error messaging

--- src/pc.c
+++ src/pc.c
@@ -1,4 +1,7 @@
 #include <stdio.h>
+#define bool unsigned char
+#define true 1
+#define false 0
 
 void usage(){
 	printf("Usage: pc <inputfile> <outputfile>\n");
@@ -14,13 +17,14 @@ int main(int argc, char ** argv){
 	FILE * infile = fopen(argv[1], "r");
 	FILE * outfile = fopen(argv[2], "w");
 	if(infile == NULL){
-		printf("cannot find %s: No such file or directory or lacks read permission", argv[1]);
+		printf("cannot find %s: No such file or directory or lacks read permission\n", argv[1]);
 	}
 	if(outfile == NULL){
-		printf("cannot open %s: No such file or directory or lacks write permission", argv[2]);
+		printf("cannot open %s: No such file or directory or lacks write permission\n", argv[2]);
 	}
 	if(infile == NULL || outfile == NULL) return 1;
 
+	bool success = true;
 	char c = (char) fgetc(infile);
 	unsigned long int open_brackets = 0;
 	unsigned long int closed_brackets = 0;
@@ -47,12 +51,44 @@ int main(int argc, char ** argv){
 				fprintf(outfile, "}\n");
 				closed_brackets++;
 				break;
+			
+			case '!':
+				fprintf(outfile, "putchar(*ptr);\n");
+				break;
+			case '?':
+				fprintf(outfile, "*ptr = getchar();\n");
+				break;
+
+			case '\n':
+				break;
+			case '\r':
+				break;
+			case '\t':
+				break;
+			case ' ':
+				break;
+
 			default:
+				printf("unknown character: %c\n", c);
+				success = false;
 				break;
 		}
 
 		c = fgetc(infile);
 	}
 
+	if(open_brackets != closed_brackets){
+		success = false;
+		printf("unbalanced brackets, found %lu \"[\" but %lu \"]\"\n", open_brackets, closed_brackets);
+	}
+
+	if(success == false){
+		fclose(infile);
+		fclose(outfile);
+		return 1;
+	}
+
+	fclose(infile);
+	fclose(outfile);
 	return 0;
 }
